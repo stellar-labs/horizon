9603292dd0a01d3288aa27c6706b04eb46f696ab
Khalyomede <khalyomede@gmail.com>
2020-03-29 17:22:11 +0200

    added configurable folder name for controllers and commands folders

--- core/import.php
+++ core/import.php
@@ -2,17 +2,20 @@
 
 namespace Horizon;
 
+use function Horizon\configuration;
 use function Horizon\listDirectoryRecursive;
 
 if (!function_exists("import")) {
 	function import() {
-		$files = listDirectoryRecursive(__DIR__ . "/../pages");
-		
+		$controllerFolderName = configuration("controller.folder");
+		$commandFolderName = configuration("command.folder");
+		$files = listDirectoryRecursive(__DIR__ . "/../$controllerFolderName");
+
 		foreach ($files as $file) {
 			require_once $file;
 		}
 
-		$files = listDirectoryRecursive(__DIR__ . "/../commands");
+		$files = listDirectoryRecursive(__DIR__ . "/../$commandFolderName");
 		
 		foreach ($files as $file) {
 			require_once $file;
