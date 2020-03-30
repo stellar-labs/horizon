0910c68a4508f14479df35d2cfbb9173ec80ef76
Khalyomede <khalyomede@gmail.com>
2020-03-30 22:23:39 +0200

    only files ending with .test will be tested, added report at the end of the tests

--- commands/test.php
+++ commands/test.php
@@ -3,21 +3,40 @@
 namespace Command;
 
 use Exception;
+use function Horizon\endsWith;
 use function Horizon\listDirectoryRecursive;
 
 if (!function_exists("test")) {
 	function test() {
-		
+		$GLOBALS["numberOfFailures"] = 0;
+		$totalDuration = 0;
+
 		assert_options(ASSERT_ACTIVE, true);
 		assert_options(ASSERT_WARNING,  false);
 		assert_options(ASSERT_CALLBACK, function($file, $line, $code, $message = null) {
-			throw new Exception("assertion failed on file $file in line $line: $message");	
+			$GLOBALS["numberOfFailures"]++;
+
+			echo $message . PHP_EOL;
 		});
 
-		$files = listDirectoryRecursive(__DIR__ . "/../test");
+		$files = array_filter(listDirectoryRecursive(__DIR__ . "/../test"), function($path) {
+			return endsWith($path, ".test.php");
+		});
 		
 		foreach ($files as $file) {
+			$start = microtime(true);
+			
 			include_once $file;
+
+			$totalDuration += microtime(true) - $start;
+		}
+
+		$totalDuration = round($totalDuration, 4);
+
+		echo "number of failures: {$GLOBALS["numberOfFailures"]}, duration: $totalDuration ms." . PHP_EOL;
+
+		if ($GLOBALS["numberOfFailures"] > 0) {
+			exit(1);
 		}
 	}
 }
