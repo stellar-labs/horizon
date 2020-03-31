7596c204a41fd07ce2b70562cf9b9b6a843eb320
Khalyomede <khalyomede@gmail.com>
2020-03-31 20:35:05 +0200

    fixed a bug when PHP variables would not be evaluated, added parameters features

--- core/view.php
+++ core/view.php
@@ -5,7 +5,7 @@ namespace Horizon;
 use function Horizon\response;
 
 if (!\function_exists("view")) {
-	function view(string $path) {
+	function view(string $path, $parameters) {
 		$folderName = configuration("view.folder");
 		$filePath = str_replace(".", DIRECTORY_SEPARATOR, $path);
 		$file = __DIR__ . "/../$folderName/$filePath.php";
@@ -14,6 +14,16 @@ if (!\function_exists("view")) {
 			throw new Exception("You used the view function with $path, but this view is not found");
 		}
 
-		return response(["text" => file_get_contents($file), "status" => 200]);
+		foreach ($parameters as $key => $value) {
+			$$key = $value;
+		}
+
+		ob_start();
+
+		include $file;
+
+		$content = ob_get_clean();
+
+		return response(["text" => $content, "status" => 200]);
 	}
 }
